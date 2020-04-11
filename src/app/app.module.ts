356e823eff6f381f5933b9665c04a7ef6f622fc1
Melinda Marino <melindamarino22@gmail.com>
2020-04-11 16:48:16 -0500

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
