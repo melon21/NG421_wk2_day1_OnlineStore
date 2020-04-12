b576dc505215472f9a41f9ae992c2364e284d1a9
Melinda Marino <melindamarino22@gmail.com>
2020-04-11 22:13:11 -0500

    Add product service

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { IProduct } from '../models/iproduct';
+import { products } from '../products';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  shoppingCart: Array<IProduct>;
+  products: Array<IProduct>;
+
+  constructor() { 
+    this.products = products;
+  }
+  getProducts(): Array<IProduct>{
+    return this.products;
+  }
+  getShoppingCart(): Array<IProduct>{
+    return this.shoppingCart;
+  }
+  addToCart(product: IProduct){
+    this.shoppingCart.push(product);
+  }
+}
