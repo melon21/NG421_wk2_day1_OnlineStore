6c11fe7fca33021bf6d9754441ff1d1db7f5f0c3
Melinda Marino <melindamarino22@gmail.com>
2020-04-13 05:00:08 -0500

    finished

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { stringify } from 'querystring';
 import {Input} from '@angular/core';
 import { products } from '../products';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -9,14 +10,16 @@ import { products } from '../products';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  
+  productWasAdded(product:Product)
+  {
+    addToCart(ProductService);
+  }
 @Input() 
 price: string;
 name: string;
 description: string;
 reviews: number;
 rating: number;
-
   constructor() {
 
    }
